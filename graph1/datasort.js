d17580592fa4a3b33ad4f3e21fe39017218ec078
Silja Walenius <siljawalenius@gmail.com>
2022-07-01 11:11:09 -0400

    minor clean up

--- graph1/datasort.js
+++ graph1/datasort.js
@@ -1,8 +1,8 @@
 const testTime = new Date(1656683511185);
 let csvData = null;
 
-const getCsvData = async () => {
-  const csvData = await d3.csv("../data/outside_sun_campus.csv", function (d) {
+const getCsvData = () => {
+  const csvData =  d3.csv("../data/outside_sun_campus.csv", function (d) {
     return {
       temp: +d.temp,
       humidity: +d.humidity,
