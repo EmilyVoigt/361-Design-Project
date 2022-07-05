1da9036f6a08bece2522a606f426c7bbf829870c
awaterston01 <73081562+awaterston01@users.noreply.github.com>
2022-07-05 13:15:28 -0400

    update with Silja's changes

--- schedule-graph/script.js
+++ schedule-graph/script.js
@@ -1,14 +1,27 @@
-// select svg tag
-let margin = { top: 10, right: 30, bottom: 30, left: 60 };
-let graphPadding = 40;
-let w = 860 - margin.left - margin.right;
-let h = 1000 - margin.top - margin.bottom;
+console.log('loaded schedule graph');
+
+// constants for formatting line graphs over the schedule image
+const width = 860;
+const height = 500;
 let numGraphs = 5;
+let graphPadding = 40;
+
+// note these formatting values are unit-scaled and multiplied by the dimensions
+// to update these values for a new schedule image, measure the pixel lengths using figma
+// and divide by the pixel size of the iamge
+const widthFactor = width / 523; // change to pixel width used for measurements
+const heightFactor = height / 895; // same here
+
+const margin = { top: 16 * heightFactor, right: 27 * widthFactor, bottom: 15 * heightFactor, left: 15 * widthFactor };
+const hourLength = 60 * widthFactor;
+const dayLength = 174 * heightFactor;
+const spaceBetweenGraphs = 0;
+
 
 const svg = d3
   .select("svg")
-  .attr("width", w + margin.left + margin.right)
-  .attr("height", h + margin.top + margin.bottom + (5*graphPadding))
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom + (5*graphPadding))
   .append("g")
   .attr("transform", `translate(${margin.left} , ${margin.top})`);
 
@@ -34,8 +47,8 @@ let curDataVal = 0;
 
     let graphGroup = svg.append("g")
         .attr("class", "dayGraph")
-        .attr("height", h/5)
-        .attr("transform", `translate(0, ${(h/numGraphs + graphPadding)  * i})`)
+        .attr("height", height/5)
+        .attr("transform", `translate(0, ${(height/numGraphs + graphPadding)  * i})`)
 
     //draw y axis
     let y = d3
@@ -46,31 +59,8 @@ let curDataVal = 0;
           return d.value;
         }),
       ])
-      .range([h/5, 0]);
-    graphGroup.append("g")
-        .attr("class", "left-axis")
-        .call(d3.axisLeft(y))
-        
-
-  //draw x axis 
-  let x = d3
-    .scaleTime()
-    .domain(
-      d3.extent(csvData, (d, i) => {
-        return d.time;
-      })
-    ) //d3.extent returns [min,max] of input
-    //manually set these values to min max dates 
-    //so x axis always starts at 8:00AM and goes to 5:00PM ON THE SELECTED DAY 
-    .range([0, w]);
-
-    //draw bottom axis
-  graphGroup
-    .append("g")
-    .attr("class", "bottom-axis")
-    .attr("transform", `translate(0, ${h/numGraphs})`)
-    .call(d3.axisBottom(x));
-
+      .range([height, 0]);
+    svg.append("g").attr("class", "left-axis").call(d3.axisLeft(y));
     //reusable draw data path function
     graphGroup
       .append("path")
@@ -93,7 +83,23 @@ let curDataVal = 0;
   };
 
 
+  //x axis does not change 
+  let x = d3
+    .scaleTime()
+    .domain(
+      d3.extent(csvData, (d, i) => {
+        return d.time;
+      })
+    ) //d3.extent returns [min,max] of input
+    //manually set these values to min max dates 
+    //so x axis always starts at 8:00AM and goes to 5:00PM ON THE SELECTED DAY 
+    .range([0, width]);
 
+  svg
+    .append("g")
+    .attr("class", "bottom-axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
 
  //initial graph
  const drawAllGraphs = (graphData)=>{
