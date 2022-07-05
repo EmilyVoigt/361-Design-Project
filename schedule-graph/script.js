a7e65b28f3465c6788cd086db3536ba7260c33c4
Silja Walenius <siljawalenius@gmail.com>
2022-07-05 15:26:09 -0400

    make graph times uniform

--- schedule-graph/script.js
+++ schedule-graph/script.js
@@ -4,11 +4,13 @@ let graphPadding = 40;
 let w = 860 - margin.left - margin.right;
 let h = 1000 - margin.top - margin.bottom;
 let numGraphs = 5;
+const startHour = 8; //start all graphs at 8am
+const endHour = 18; //end all graphs at 6pm
 
 const svg = d3
   .select("svg")
   .attr("width", w + margin.left + margin.right)
-  .attr("height", h + margin.top + margin.bottom + (5*graphPadding))
+  .attr("height", h + margin.top + margin.bottom + 5 * graphPadding)
   .append("g")
   .attr("transform", `translate(${margin.left} , ${margin.top})`);
 
@@ -20,63 +22,100 @@ const container = svg
 const daySelectTag = document.querySelector("select.daySelect");
 const dataSelectTag = document.querySelector("select.dataSelect");
 
-let curDay = classTimes[0];
-let curDataVal = 0;
+//let curDay = classTimes[0];
+let dataSelectVal = 0;
 
 (async () => {
   let csvData = await getCsvData();
   const lightTimeData = getLightTimeData(csvData);
   const tempTimeData = getTempTimeData(csvData);
   const humidityTimeData = getHumidityTimeData(csvData);
+  const mappedDays = getDataDays(csvData); // array of days we actually have data for
 
-  const drawData = (data, curDataVal, i ) => {
-    let colors = ["green", "orange", "steelblue"]
+  //draw data function
+  const drawData = (data, dataSelectVal, i, curDay) => {
+    let colors = ["green", "orange", "steelblue"];
+    const startTime = new Date(
+      curDay.year,
+      curDay.month,
+      curDay.date,
+      startHour
+    );
+    const endTime = new Date(curDay.year, curDay.month, curDay.date, endHour);
 
-    let graphGroup = svg.append("g")
-        .attr("class", "dayGraph")
-        .attr("height", h/5)
-        .attr("transform", `translate(0, ${(h/numGraphs + graphPadding)  * i})`)
+    //now, filter the data based on selected day
+
+    console.log(startTime, endTime)
+
+    const dateData = [];
+    data.forEach((point) => {
+      // we pass in 2 variables of data to this function
+      let pointDate = point.time.getDate();
+      let pointYear = point.time.getFullYear();
+      let pointMonth = point.time.getMonth();
+
+      //here, we filter the data by day, adding only relevent day data to the curdata array
+      if (
+        pointDate === curDay.date &&
+        pointYear === curDay.year &&
+        pointMonth === curDay.month
+      ) {
+        dateData.push(point);
+      }
+    });
+
+    console.log(dateData)
+
+
+    let graphGroup = svg
+      .append("g")
+      .attr("class", "dayGraph")
+      .attr("height", h / 5)
+      .attr("transform", `translate(0, ${(h / numGraphs + graphPadding) * i})`);
 
     //draw y axis
     let y = d3
       .scaleLinear()
       .domain([
         0,
-        d3.max(data, (d, i) => {
+        d3.max(dateData, (d, i) => {
           return d.value;
         }),
       ])
-      .range([h/5, 0]);
-    graphGroup.append("g")
-        .attr("class", "left-axis")
-        .call(d3.axisLeft(y))
-        
+      .range([h / 5, 0]);
+    graphGroup.append("g").attr("class", "left-axis").call(d3.axisLeft(y));
 
-  //draw x axis 
-  let x = d3
-    .scaleTime()
-    .domain(
-      d3.extent(csvData, (d, i) => {
-        return d.time;
-      })
-    ) //d3.extent returns [min,max] of input
-    //manually set these values to min max dates 
-    //so x axis always starts at 8:00AM and goes to 5:00PM ON THE SELECTED DAY 
-    .range([0, w]);
+    //draw x axis
+    let x = d3
+      .scaleTime()
+    //   .domain(
+    //     d3.extent(dateData, (d, i) => {
+    //       return d.time;
+    //     })
+    //   )
+      .domain([startTime, endTime])
+      .range([0, w]);
 
     //draw bottom axis
-  graphGroup
-    .append("g")
-    .attr("class", "bottom-axis")
-    .attr("transform", `translate(0, ${h/numGraphs})`)
-    .call(d3.axisBottom(x));
+    graphGroup
+      .append("g")
+      .attr("class", "bottom-axis")
+      .attr("transform", `translate(0, ${h / numGraphs})`)
+      .call(d3.axisBottom(x));
+
+    graphGroup
+      .append("text")
+      .text(`${curDay.day}`)
+      .attr("class", "xlabel")
+      .attr("color", "black")
+      .attr("transform", `translate(0, ${h / numGraphs})`);
 
     //reusable draw data path function
     graphGroup
       .append("path")
-      .datum(data) //input an array of light and time here
+      .datum(dateData) //input an array of light and time here
       .attr("fill", "none")
-      .attr("stroke", colors[curDataVal])
+      .attr("stroke", colors[dataSelectVal])
       .attr("class", "dataPath")
       .attr("stroke-width", 1.5)
       .attr(
@@ -92,33 +131,31 @@ let curDataVal = 0;
       );
   };
 
-
-
-
- //initial graph
- const drawAllGraphs = (graphData)=>{
-    for (let i = 0; i < numGraphs; i++){
-        drawData(graphData, curDataVal, i);
+  //initial graph
+  const drawAllGraphs = (graphData) => {
+    for (let i = 0; i < mappedDays.length; i++) {
+      drawData(graphData, dataSelectVal, i, mappedDays[i]);
     }
- }
+  };
 
- drawAllGraphs(lightTimeData)
+  drawAllGraphs(lightTimeData);
 
   //event listener for type of data to draw
   dataSelectTag.addEventListener("input", () => {
-    curDataVal = dataSelectTag.value;
+    dataSelectVal = dataSelectTag.value;
 
-    d3.selectAll(".dataPath").remove();
-    d3.selectAll(".left-axis").remove();
+    d3.selectAll(".dataPath").remove(); // remove all existing graphs
+    d3.selectAll(".left-axis").remove(); //remove left axis
+    d3.selectAll(".bottom-axis").remove(); //remove bottom axis
 
-    if (curDataVal == 0) {
-        drawAllGraphs(lightTimeData)
+    if (dataSelectVal == 0) {
+      drawAllGraphs(lightTimeData);
     }
-    if (curDataVal == 1) {
-        drawAllGraphs(tempTimeData)
+    if (dataSelectVal == 1) {
+      drawAllGraphs(tempTimeData);
     }
-    if (curDataVal == 2) {
-        drawAllGraphs(humidityTimeData)
+    if (dataSelectVal == 2) {
+      drawAllGraphs(humidityTimeData);
     }
   });
 })();
