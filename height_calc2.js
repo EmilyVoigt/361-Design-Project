fe5ff12305605e3d97a63949401cdec27ca30d68
Jessica Wormald <jessica.wormald@emetros.com>
2023-03-05 23:03:55 -0500

    another clinometer

---
+++ height_calc2.js
@@ -0,0 +1,35 @@
+var theForm = document.forms["calc_form2"];
+
+function getEyeheight() {
+    var eyeheight = theForm.elements["eyeheight"];
+    var eyevalue =0;
+    //If the textbox is not blank
+    if(eyeheight.value!="")
+    {
+        eyevalue = Number(eyeheight.value);
+    }
+    return eyevalue;
+}
+
+function getAngle() {
+    var angle = theForm.elements["angle"];
+    var anglevalue =0;
+    //If the textbox is not blank
+    if(angle.value!="")
+    {
+        anglevalue = Number(angle.value);
+    }
+    return anglevalue;
+}
+
+
+function calculateHeight () {
+    eyeheight = getEyeheight();
+    angle = getAngle();
+
+    height = 200*(Math.tan(angle)) + eyeheight;
+
+
+    document.getElementById('height').innerHTML = height + " cm";
+
+}
